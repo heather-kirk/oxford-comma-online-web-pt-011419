399b8991ef1027659e7532e55ef9859ce8821cd5
Heather Kirk <hskirk@gmail.com>
2019-01-22 02:06:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,6 @@ array.join
 elsif array.length == 2
 array.join(" and ")
 else array.length >= 3
-  array[0..-2].join(", ") + ", and" array[-1]
+  array[0..2].join(", ") + ", and" array[-1]
   end
-end
 end

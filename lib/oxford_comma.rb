668e141966a96698fcf40b21608227ce1392c63c
Heather Kirk <hskirk@gmail.com>
2019-01-22 02:11:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,12 +4,6 @@ def oxford_comma(array)
   when 2 
   array.join
   when 3 
-    
-  if array.length == 1
-array.join
-elsif array.length == 2
-array.join(" and ")
-else array.length >= 3
   array[0..2].join(", ") + ", and" array[-1]
   end
 end
